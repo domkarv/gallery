b79854346bccc501ce4a9b4b037c1f14e1498a8f
Omkar Date <omkardate29@gmail.com>
2024-05-06 20:48:03 +0530

    ops: deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      Friends Gallery App
+      <h2 className="text-xl font-semibold">{`Friend's Gallery App`}</h2>
     </main>
   );
 }
