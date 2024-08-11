4064ea102036e680cae6950854b12f25a902c49f
Omkar Date <omkardate29@gmail.com>
2024-08-11 21:58:46 +0530

    fix: image actions

--- src/server/image-actions.ts
+++ src/server/image-actions.ts
@@ -2,12 +2,12 @@
 
 import { auth } from "@clerk/nextjs/server";
 import { v2 as cloudinary } from "cloudinary";
-import { and, eq } from "drizzle-orm";
+import { and, eq, sql } from "drizzle-orm";
 import { type CloudinaryUploadWidgetInfo } from "next-cloudinary";
 import { revalidatePath } from "next/cache";
 import { env } from "~/env";
 import { db } from "./db";
-import { type ImageType, images } from "./db/schema";
+import { type ImageType, groups, images } from "./db/schema";
 
 export async function getImages(): Promise<ImageType[]> {
   const user = auth();
@@ -16,10 +16,17 @@ export async function getImages(): Promise<ImageType[]> {
     throw new Error("Unauthorized!");
   }
 
-  return await db.query.images.findMany({
-    where: (model, { eq }) => eq(model.userId, user.userId),
-    orderBy: (model, { desc }) => desc(model.createdAt),
-  });
+  try {
+    return await db.query.images.findMany({
+      where: (model, { eq }) => eq(model.userId, user.userId),
+      orderBy: (model, { desc }) => desc(model.createdAt),
+    });
+  } catch (error) {
+    if (error instanceof Error) console.error(error.message);
+    else console.error(error);
+
+    throw new Error("Error occured while reading images");
+  }
 }
 
 export async function getImage(id: string) {
@@ -29,10 +36,17 @@ export async function getImage(id: string) {
     throw new Error("Unauthorized!");
   }
 
-  return await db.query.images.findFirst({
-    where: (model, { and, eq }) =>
-      and(eq(model.id, id), eq(model.userId, user.userId)),
-  });
+  try {
+    return await db.query.images.findFirst({
+      where: (model, { and, eq }) =>
+        and(eq(model.id, id), eq(model.userId, user.userId)),
+    });
+  } catch (error) {
+    if (error instanceof Error) console.error(error.message);
+    else console.error(error);
+
+    throw new Error("Error occured while reading image");
+  }
 }
 
 export async function deleteImage(id: string) {
@@ -42,20 +56,27 @@ export async function deleteImage(id: string) {
     throw new Error("Unauthorized!");
   }
 
-  await db
-    .delete(images)
-    .where(and(eq(images.publicId, id), eq(images.userId, user.userId)))
-    .returning({ url: images.url });
+  try {
+    await db
+      .delete(images)
+      .where(and(eq(images.publicId, id), eq(images.userId, user.userId)))
+      .returning({ url: images.url });
 
-  cloudinary.config({
-    api_key: env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
-    cloud_name: env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
-    api_secret: env.CLOUDINARY_API_SECRET,
-  });
+    cloudinary.config({
+      api_key: env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
+      cloud_name: env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+      api_secret: env.CLOUDINARY_API_SECRET,
+    });
 
-  await cloudinary.uploader.destroy(id, () => {
-    console.log("Image Deleted!");
-  });
+    await cloudinary.uploader.destroy(id, () => {
+      console.log("Image Deleted!");
+    });
+  } catch (error) {
+    if (error instanceof Error) console.error(error.message);
+    else console.error(error);
+
+    throw new Error("Error occured while deleting image");
+  }
 
   revalidatePath("/");
 }
@@ -77,13 +98,34 @@ export async function uploadImage({
     throw new Error("Failed to read images!");
   }
 
-  await db.insert(images).values({
-    url: info.url,
-    name: info.original_filename,
-    userId: user.userId,
-    publicId: info.public_id,
-    groupId: groupId,
-  });
+  try {
+    const image = await db
+      .insert(images)
+      .values({
+        url: info.url,
+        name: info.original_filename,
+        userId: user.userId,
+        publicId: info.public_id,
+        groupId: groupId,
+      })
+      .returning({
+        id: images.id,
+      });
+
+    await db
+      .update(groups)
+      .set({
+        images: sql`array_append(${groups.images}, ${image[0]!.id})`,
+      })
+      .where(eq(groups.id, groupId));
+  } catch (error) {
+    if (error instanceof Error) console.error(error.message);
+    else console.error(error);
+
+    throw new Error(
+      "Error occured while uploadin image and updating group info",
+    );
+  }
 
   revalidatePath("/");
 }
