7ea485a40f134d54a713f2282d7fa3ef2ab2deea
Omkar Date <omkardate29@gmail.com>
2024-08-12 01:04:58 +0530

    feat: ui componets

--- src/components/group-card.tsx
+++ src/components/group-card.tsx
@@ -1,11 +1,30 @@
-import { Card } from "~/components/ui/card";
-import Link from "next/link";
+import { EllipsisVerticalIcon } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "~/components/ui/alert-dialog";
+import { Card } from "~/components/ui/card";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "~/components/ui/dropdown-menu";
 import { type GroupType } from "~/server/db/schema";
 
 export default function GroupCard({ group }: { group: GroupType }) {
   return (
-    <Card className="w-full max-w-md overflow-hidden rounded-xl shadow-lg">
+    <Card className="relative w-full max-w-md overflow-hidden rounded-xl shadow-lg">
       <Link
         href={`/group/${group.id}`}
         className="group relative block"
@@ -27,6 +46,36 @@ export default function GroupCard({ group }: { group: GroupType }) {
           <h3 className="text-2xl font-semibold">{group.name}</h3>
         </div>
       </Link>
+
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <EllipsisVerticalIcon className="absolute right-3 top-3 cursor-pointer stroke-black" />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem>Change Thumbnail</DropdownMenuItem>
+          <DropdownMenuItem>Rename</DropdownMenuItem>
+          <AlertDialog>
+            <AlertDialogTrigger>
+              <DropdownMenuLabel className="text-destructive">
+                Delete
+              </DropdownMenuLabel>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+                <AlertDialogDescription>
+                  This action cannot be undone. This will permanently delete
+                  your group and remove your data from our servers.
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                <AlertDialogAction>Continue</AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </Card>
   );
 }
